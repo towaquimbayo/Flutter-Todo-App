192c38b556fc4b5c65f874a7f274e4f82ca191eb
Towa Quimbayo <towacurtis@hotmail.com>
2024-06-20 17:45:10 -0700

    Created custom dialog popup component

---
+++ lib/utils/dialog_popup.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:todo_app/utils/button.dart';
+
+class DialogPopup extends StatelessWidget {
+  final controller;
+  final VoidCallback onSave;
+  final VoidCallback onCancel;
+
+  const DialogPopup({
+    super.key,
+    required this.controller,
+    required this.onSave,
+    required this.onCancel,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      backgroundColor: Colors.yellow,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10),
+      ),
+      content: Container(
+        height: 120,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            TextField(
+              controller: controller,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: "Add a new task",
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                MyButton(text: "Save", onPressed: onSave),
+                const SizedBox(width: 10),
+                MyButton(text: "Cancel", onPressed: onCancel),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
