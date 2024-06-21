84fc27534df9672acd79f1e548b3e826c7ab3ceb
Towa Quimbayo <towacurtis@hotmail.com>
2024-06-20 17:45:25 -0700

    Added add new task floating button

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todo_app/utils/dialog_popup.dart';
 import 'package:todo_app/utils/todo_task.dart';
 
 class HomePage extends StatefulWidget {
@@ -9,6 +10,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final _controller = TextEditingController();
   List todoList = [
     ['Study Java', false],
     ['Make a Flutter App', true],
@@ -17,10 +19,34 @@ class _HomePageState extends State<HomePage> {
     ['Learn Algorithms', false],
   ];
 
+  // Function to change the status of the Task checkbox
   void checkboxChanged(bool? value, int index) {
     setState(() {
       todoList[index][1] = value!;
     });
+  }
+
+  // Function to save the new task
+  void saveNewTask() {
+    setState(() {
+      todoList.add([_controller.text, false]);
+      _controller.clear();
+    });
+    Navigator.of(context).pop();
+  }
+
+  // Function to create a new task
+  void createNewTask() {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return DialogPopup(
+          controller: _controller,
+          onSave: saveNewTask,
+          onCancel: () => Navigator.of(context).pop(),
+        );
+      },
+    );
   }
 
   @override
@@ -32,6 +58,11 @@ class _HomePageState extends State<HomePage> {
         title: const Text('Todo App'),
         centerTitle: true,
         elevation: 0,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: createNewTask,
+        backgroundColor: Colors.yellow,
+        child: const Icon(Icons.add),
       ),
       body: ListView.builder(
         itemCount: todoList.length,
